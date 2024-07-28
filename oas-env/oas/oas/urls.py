0d41426b41158cf69babd31bb9c35178e6d62ade
Usman Ali <usmanaakhan22@gmail.com>
2024-07-29 02:47:44 +0500

    .

--- oas-env/oas/oas/urls.py
+++ oas-env/oas/oas/urls.py
@@ -20,6 +20,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='home'),
+    path('', views.index,),
     path('about/', views.aboutPage, name='about'),
 ]
