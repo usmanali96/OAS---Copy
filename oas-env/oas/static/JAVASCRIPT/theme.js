e78da71b72469cc7c8f4db893d2ceb2951d015ce
Usman Ali <usmanaakhan22@gmail.com>
2024-08-06 01:32:36 +0500

    .

--- oas-env/oas/static/JAVASCRIPT/theme.js
+++ oas-env/oas/static/JAVASCRIPT/theme.js
@@ -44,6 +44,16 @@ $('.owl-carousel-1').owlCarousel({
 
 
 
+          $(".slider").owlCarousel({
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: 2000, //2000ms = 2s;
+            autoplayHoverPause: true,
+          });
+
+
+
+
 
 
 
