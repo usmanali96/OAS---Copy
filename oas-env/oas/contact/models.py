c3007ccdc475927d0227f9777be24911618b1317
Usman Ali <usmanaakhan22@gmail.com>
2024-08-16 02:48:26 +0500

    .

--- oas-env/oas/contact/models.py
+++ oas-env/oas/contact/models.py
@@ -8,3 +8,7 @@ class Contact(models.Model):
     phone = models.CharField(max_length=13)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return 'Message from' + self.name
