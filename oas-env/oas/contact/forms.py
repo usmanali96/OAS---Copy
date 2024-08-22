4e6cd2c3285597603f872941073914e6c0b706a0
Usman Ali <usmanaakhan22@gmail.com>
2024-08-22 10:49:06 +0500

    .

--- oas-env/oas/contact/forms.py
+++ oas-env/oas/contact/forms.py
@@ -3,12 +3,11 @@ from django import forms
 from contact.models import Contact
 
 
-class contactForm(forms.contactForm):
+class contactForm(forms.ModelForm):
     name = forms.CharField(max_length=30)
     email = forms.CharField(max_length=100)
     phone = forms.CharField(max_length=13)
     content = forms.CharField()
-
 
     class Meta:
         model = Contact
