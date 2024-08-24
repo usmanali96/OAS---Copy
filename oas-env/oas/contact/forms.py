a769ba86ad7bf260a6bfca72c3c2e6dd6e7caffc
Usman Ali <usmanaakhan22@gmail.com>
2024-08-25 01:12:32 +0500

    .

--- oas-env/oas/contact/forms.py
+++ oas-env/oas/contact/forms.py
@@ -4,10 +4,10 @@ from contact.models import Contact
 
 
 class contactForm(forms.Form):
-    name = forms.CharField(max_length=30, label='Your Name')
-    email = forms.CharField(max_length=100, label='Your Email')
-    phone = forms.CharField(max_length=13, label='Your Phone')
-    content = forms.CharField(widget=forms.Textarea, label='Your Message')
+    name = forms.CharField(max_length=30, label='Name')
+    email = forms.CharField(max_length=100, label='Email')
+    phone = forms.CharField(max_length=13, label='Phone')
+    content = forms.CharField(widget=forms.Textarea, label='Message')
 
     class Meta:
         model = Contact
