8376cc09fb8c672fdd21a1ac2ae92036559039ed
Usman Ali <usmanaakhan22@gmail.com>
2024-08-26 14:19:46 +0500

    .

--- oas-env/oas/oas/views.py
+++ oas-env/oas/oas/views.py
@@ -68,16 +68,15 @@ def registerUser(request):
         return render(request, 'register.html')
 
 
-def registerUser(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/')  
-    else:
-        form = UserCreationForm()
-    admin.site.register(User)
-    return render(request, 'register.html', {'form': form})
+#def registerUser(request):
+    #if request.method == 'POST':
+        #form = UserCreationForm(request.POST)
+        #if form.is_valid():
+           # form.save()
+           # return redirect('/')  
+    #else:
+      #  form = UserCreationForm()
+   # return render(request, 'register.html', {'form': form})
 
 
 
