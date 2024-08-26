eb178965233c5fbc990408d4a98da87669f17e8e
Usman Ali <usmanaakhan22@gmail.com>
2024-08-26 13:56:09 +0500

    .

--- oas-env/oas/oas/views.py
+++ oas-env/oas/oas/views.py
@@ -74,7 +74,7 @@ def loginUser(request):
         upassword = request.POST.get('password')
         user = authenticate(request, username=uname, password=upassword)
         if user is not None:
-             login(request)
+             login(request, user)
              return redirect('/')
         else:
              print("user doesn't exist")
