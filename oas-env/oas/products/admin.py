f91a82f24a7615a7a04204b6fe9ded929da0e27e
Usman Ali <usmanaakhan22@gmail.com>
2024-08-29 01:17:04 +0500

    .

--- oas-env/oas/products/admin.py
+++ oas-env/oas/products/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from .models import Products
-
+from .models import Product
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'category', 'image']
     
     
-admin.site.register(Products, ProductAdmin) 
+admin.site.register(Product, ProductAdmin) 
 
 
 # Register your models here.
