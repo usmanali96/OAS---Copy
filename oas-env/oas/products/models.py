c5171ba590facc9d05fe99ac8df9db4c63a99b17
Usman Ali <usmanaakhan22@gmail.com>
2024-09-01 04:08:17 +0500

    .

--- oas-env/oas/products/models.py
+++ oas-env/oas/products/models.py
@@ -7,6 +7,8 @@ class Product(models.Model):  # Rename from Products to Product
     category = models.CharField(max_length=60)
     image = models.FileField(max_length=60, upload_to="products/", null=True)
     bid_end_time = models.DateTimeField(null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
 
     def __str__(self):
         return self.title
