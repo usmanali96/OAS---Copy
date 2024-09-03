851d785bebfc42393c4ef37b04c73915163827df
Usman Ali <usmanaakhan22@gmail.com>
2024-09-03 15:37:49 +0500

    .

--- oas-env/oas/oas/views.py
+++ oas-env/oas/oas/views.py
@@ -13,7 +13,9 @@ from datetime import datetime
 from django.contrib import admin
 from django.shortcuts import render, get_object_or_404, redirect
 from products.models import Product
-from django.core.exceptions import ValidationError
+from django.core.mail import send_mail
+from datetime import datetime, timedelta
+from django.template.loader import render_to_string
 
 
 
@@ -132,6 +134,15 @@ def contactPage(request):
 
 
 
+
+
+def send_bid_end_email(product, bid):
+    # Send email to the bid email
+    subject = 'Bid End Notification'
+    message = render_to_string('bid_end_email.html', {'product': product, 'bid': bid})
+    send_mail(subject, message, 'your_email@example.com', [bid['email']])
+
+
 def save_price(request, product_id):
     
     
@@ -152,6 +163,16 @@ def save_price(request, product_id):
         product.bids.append(new_bid)
         product.save()
 
+
+        # Record bid end time
+        bid_end_time = datetime.now() + timedelta(minutes=10)  # Change this to your desired bid end time
+        product.bid_end_time = bid_end_time
+        product.save()
+        
+        # Call the send_bid_end_email function directly
+        send_bid_end_email(product, new_bid)
+
+
         return redirect('product_detail', product_id=product_id)
     
     return render(request, 'product_detail.html', {'product': product})
@@ -163,3 +184,8 @@ def save_price(request, product_id):
 
 def browse_page(request):
     return render(request, 'browse_product.html')
+
+
+
+
+
