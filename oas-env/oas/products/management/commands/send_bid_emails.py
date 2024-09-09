263167ef7ecccda8d68dc716f8cba179ef08c26e
Usman Ali <usmanaakhan22@gmail.com>
2024-09-10 02:01:23 +0500

    .

--- oas-env/oas/products/management/commands/send_bid_emails.py
+++ oas-env/oas/products/management/commands/send_bid_emails.py
@@ -6,6 +6,7 @@ from products.models import Product
 class Command(BaseCommand):
     help = 'Send an email to the highest bidder when the bid end time has passed'
 
+
     def handle(self, *args, **kwargs):
         now = timezone.now()
         products = Product.objects.filter(bid_end_time__lte=now, email_sent=False)
@@ -16,19 +17,13 @@ class Command(BaseCommand):
                 email = highest_bid.get('email')
                 
                 if email:
-                    try:
-                        send_mail(
-                            subject=f'Bid Winner for {product.title}',
-                            message=f'Congratulations! You have the highest bid of {highest_bid["price"]} for {product.title}. Congrats for winning the Auction. We will share the payment details soon.',
-                            from_email='onlineauction537@gmail.com',
-                            recipient_list=[email],
-                        )
-                        product.email_sent = True  # Mark email as sent
-                        product.save()
-                        self.stdout.write(self.style.SUCCESS(f'Successfully sent email to {email} for product {product.title}.'))
-                    except Exception as e:
-                        self.stdout.write(self.style.ERROR(f'Failed to send email: {e}'))
-                else:
-                    self.stdout.write(self.style.ERROR(f'No email found for highest bid on product {product.title}.'))
+                    send_mail(
+                        subject=f'Bid Winner for {product.title}',
+                        message=f'Congratulations! You have the highest bid of {highest_bid["price"]} for {product.title}. Congrats for winning the Auction. We will share the payment details soon.',
+                        from_email='your_email@gmail.com',
+                        recipient_list=[email],
+                    )
+                    product.email_sent = True  # Mark email as sent
+                    product.save()
 
         self.stdout.write(self.style.SUCCESS('Completed checking and sending bid end emails.'))
