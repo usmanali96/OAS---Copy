4a2acc1e09733a87fc1cca8c9f54c745627fa943
Usman Ali <usmanaakhan22@gmail.com>
2024-09-10 01:46:23 +0500

    .

--- oas-env/oas/products/management/commands/send_bid_emails.py
+++ oas-env/oas/products/management/commands/send_bid_emails.py
@@ -4,7 +4,7 @@ from django.core.mail import send_mail
 from products.models import Product
 
 class Command(BaseCommand):
-    help = 'Send emails when bid end time has passed'
+    help = 'Send an email to the highest bidder when the bid end time has passed'
 
     def handle(self, *args, **kwargs):
         now = timezone.now()
@@ -25,7 +25,10 @@ class Command(BaseCommand):
                         )
                         product.email_sent = True  # Mark email as sent
                         product.save()
+                        self.stdout.write(self.style.SUCCESS(f'Successfully sent email to {email} for product {product.title}.'))
                     except Exception as e:
                         self.stdout.write(self.style.ERROR(f'Failed to send email: {e}'))
+                else:
+                    self.stdout.write(self.style.ERROR(f'No email found for highest bid on product {product.title}.'))
 
-        self.stdout.write(self.style.SUCCESS('Successfully sent bid end emails.'))
+        self.stdout.write(self.style.SUCCESS('Completed checking and sending bid end emails.'))
