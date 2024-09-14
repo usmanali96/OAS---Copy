f48822691b4750be0ed8fdb96942266981fb5676
Usman Ali <usmanaakhan22@gmail.com>
2024-09-15 04:46:19 +0500

    .

--- oas-env/oas/oas/urls.py
+++ oas-env/oas/oas/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
     path('product/<int:product_id>/save_price/', views.save_price, name='save_price'),
     path('browse_product/', views.browse_page, name='browse_page'),
-    path('', views.include('products.urls')),  # Include 'products' app URLs
+    path('submit-review/', submit_review, name='submit_review'),
     ]
 
 
