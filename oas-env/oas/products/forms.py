a62a847fa216f7a6393ce89a7ceede2f2e46e768
Usman Ali <usmanaakhan22@gmail.com>
2024-09-15 03:30:41 +0500

    .

--- oas-env/oas/products/forms.py
+++ oas-env/oas/products/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+
+
 class ReviewForm(forms.Form):
-    name = forms.CharField(max_length=100, label="Your Name")
-    comment = forms.CharField(widget=forms.Textarea, label="Your Review")
+    name = forms.CharField(max_length=100, label="Your Name", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your Name'}))
+    comment = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Your Review', 'rows': 4}), label="Your Review")
