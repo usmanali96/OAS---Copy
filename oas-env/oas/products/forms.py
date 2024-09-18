a513fee7d24b70e583233c10121f9f1e58cefc79
Usman Ali <usmanaakhan22@gmail.com>
2024-09-18 21:00:36 +0500

    .

--- oas-env/oas/products/forms.py
+++ oas-env/oas/products/forms.py
@@ -4,5 +4,5 @@ from .models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['title', 'description', 'category', 'image', 'price', 'bid_end_time']
+        fields = ['title', 'description', 'category', 'image', 'long-description', 'bid_end_time']
         
