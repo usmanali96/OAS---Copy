bf8fd89a2abc818884af90ed41c1a53e2810d73b
Usman Ali <usmanaakhan22@gmail.com>
2024-09-21 02:18:10 +0500

    .

--- oas-env/oas/oas/settings.py
+++ oas-env/oas/oas/settings.py
@@ -167,7 +167,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'onlineauction537@gmail.com'  
-EMAIL_HOST_PASSWORD = 'slxc zznc xowo iggh'  
+EMAIL_HOST_PASSWORD = 'fzxoazdvvfnjwgfw'  
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
